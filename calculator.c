dfc61334dfd52e69bd7f4f5fcbe362e8c658aea5
gatewaybypasser <yogeshthakur7800@gmail.com>
2024-02-24 22:53:06 +0530

    multiplication!
    only supports multiplication as of now.

---
+++ calculator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int a,b; 
+    char operator[2];
+
+    printf("--First number:\n>> ");
+    scanf("%d", &a);
+    
+    printf("--Operator:\n>> ");
+    scanf("%s", &operator);
+
+    printf("--Second number:\n>> ");
+    scanf("%d", &b);
+
+    if(strcmp(operator,"*")==0)
+    {
+    printf("--Result:\n>> %d <<\n", a*b);
+    }
+
+    return 0;
+
+}
